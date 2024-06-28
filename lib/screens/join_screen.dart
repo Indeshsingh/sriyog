c1765cbfb8c787329b1f4358276c78925484642c
Indeshsingh <indeshmahato799@gmail.com>
2024-06-28 15:26:36 +0545

    added inputfields and decoration border

--- lib/screens/join_screen.dart
+++ lib/screens/join_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class JoinScreen extends StatelessWidget {
   const JoinScreen({super.key});
@@ -7,8 +8,97 @@ class JoinScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: CircleAvatar(
-          backgroundImage: AssetImage('assets/logo.png'),
+        child: Padding(
+          padding: EdgeInsets.only(left: 18.0, right: 18.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/logo.png'),
+                  radius: 90,
+                ),
+              ),
+              Text(
+                'Became a Professional',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text('NPR 1,999/- Yearly'),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Full Name *',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Middle Name *',
+                    focusColor: Colors.red,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Last Name *',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'profession *',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Phone number *',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'City *',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
