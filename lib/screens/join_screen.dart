14fc7031d1609b2e68b881ee7d2223fc3f81c73f
Indeshsingh <indeshmahato799@gmail.com>
2024-06-30 15:09:32 +0545

    added checkbox and submit button

--- lib/screens/join_screen.dart
+++ lib/screens/join_screen.dart
@@ -1,103 +1,163 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// ignore_for_file: prefer_const_constructors
 
-class JoinScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+class JoinScreen extends StatefulWidget {
   const JoinScreen({super.key});
 
   @override
+  State<JoinScreen> createState() => _JoinScreenState();
+}
+
+class _JoinScreenState extends State<JoinScreen> {
+  bool valueFirst = false; // Define the boolean variable
+  final TextEditingController _name = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.only(left: 18.0, right: 18.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/logo.png'),
-                  radius: 90,
-                ),
-              ),
-              Text(
-                'Became a Professional',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text('NPR 1,999/- Yearly'),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Full Name *',
-                    border: OutlineInputBorder(),
+          padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/logo.png'),
+                    radius: 85,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Middle Name *',
-                    focusColor: Colors.red,
-                    border: OutlineInputBorder(),
+                const Text(
+                  'Become a Professional',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text('NPR 1,999/- Yearly'),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _name,
+                    decoration: InputDecoration(
+                      labelText: 'Full Name *',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Last Name *',
-                    border: OutlineInputBorder(),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    //keyboardType: TextInputType.number,
+
+                    decoration: InputDecoration(
+                      labelText: 'Middle Name ',
+                      focusColor: Colors.red,
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'profession *',
-                    border: OutlineInputBorder(),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Last Name *',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Phone number *',
-                    border: OutlineInputBorder(),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Profession *',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'City *',
-                    border: OutlineInputBorder(),
+                const SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    focusNode: FocusNode(
+                      canRequestFocus: true,
+                    ),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number *',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              )
-            ],
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'City *',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: valueFirst,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            valueFirst = value!;
+                          });
+                        },
+                      ),
+                      const Text('I agree to the Terms and Conditions'),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        WidgetStatePropertyAll(Color.fromARGB(255, 19, 18, 18)),
+                    //mouseCursor: WidgetStatePropertyAll()
+                  ),
+                  onPressed: () {
+                    if (valueFirst) {
+                      print("Form Submitted");
+                    } else {
+                      print("Please agree to the terms and conditions");
+                    }
+                  },
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
