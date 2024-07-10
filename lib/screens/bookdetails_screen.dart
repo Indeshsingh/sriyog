31188b7a95c160823c2c5a1bd0fad9a5b1f4fec3
Indeshsingh <indeshmahato799@gmail.com>
2024-07-10 15:54:57 +0545

    added depencies and added footer in bookdetails_screen page

--- lib/screens/bookdetails_screen.dart
+++ lib/screens/bookdetails_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:profile_app/helpers/apphelper.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
 class BookdetailsScreen extends StatelessWidget {
   final Map<String, String> book;
@@ -12,52 +16,73 @@ class BookdetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(book['text']!),
       ),
-      body: InteractiveViewer(
-        panEnabled: true,
-        scaleEnabled: true,
-        minScale: 0.5,
-        maxScale: 6.0,
-        panAxis: PanAxis.free,
-        constrained: true,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Image.asset(
-                  book['imagePath']!,
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width * 0.6,
+      body: FooterView(
+        children: [
+          InteractiveViewer(
+            panEnabled: true,
+            scaleEnabled: true,
+            minScale: 0.5,
+            maxScale: 6.0,
+            panAxis: PanAxis.free,
+            constrained: true,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      book['imagePath']!,
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      AppHelpers.bookDescription[
+                          AppHelpers.bookName.indexOf(book['text']!)],
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      AppHelpers.bookSynopsis[
+                          AppHelpers.bookName.indexOf(book['text']!)],
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 65,
+                    ),
+                    ElevatedButton(
+                      style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.black),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Read Now",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                Text(
-                  AppHelpers.bookDescription[
-                      AppHelpers.bookName.indexOf(book['text']!)],
-                  style: const TextStyle(fontSize: 14),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  AppHelpers
-                      .bookSynopsis[AppHelpers.bookName.indexOf(book['text']!)],
-                  style: const TextStyle(fontSize: 14),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Colors.black),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    "Read Now",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                )
-              ],
+              ),
             ),
+          ),
+        ],
+        footer: Footer(
+          child: const Column(
+            children: [
+              Divider(
+                color: Colors.black,
+                thickness: 0.5,
+              ),
+              Text(
+                '© 2024, All Rights Reserved.',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+            ],
           ),
         ),
       ),
